d7d9d92464ea1bb17fae13291338bfe2e86773bb
Satyam Raj <rajsatyamraj03@gmail.com>
2026-04-04 10:25:30 +0530

    Create chat model with schema for MongoDB

---
+++ backend\models\chat.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema({
+  user: String,
+  message: String,
+  reply: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Chat", chatSchema);
